47e0831188cd05dbcbd46c14d8adf1bfa73b4213
YouyouzkT <usul_le_fremen@yahoo.fr>
2025-11-09 21:03:22 +0100

    build test

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
+gimport { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: 'https://YouyouzkT.github.io/zkTDrawOnchain/',   // Important pour que les chemins relatifs fonctionnent sur GitHub Pages
   plugins: [react()],
 })
